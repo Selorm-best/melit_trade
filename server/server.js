d4ec3a44b5bffc288a2f437f763d7175b27d6db4
De-General-1 <aboagyefafa12@gmail.com>
2025-09-15 22:42:14 +0000

    Change port 5001 to 5000

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5001'],
+  origin: ['http://localhost:3000', 'http://localhost:5000'],
   credentials: true,
   optionsSuccessStatus: 200
 }));
